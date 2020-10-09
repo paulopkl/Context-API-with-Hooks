18940f3016e09a2b2c72112f96477f95419e9840
paulopkl <palmeida.ipms@gmail.com>
2020-10-09 19:49:04 -0300

    Updates

---
+++ precache-manifest.27013334ec1b9b11835e23edee377ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae6a84188e6cd526dd444c007b7baa8",
+    "url": "/Context-API-with-Hooks/index.html"
+  },
+  {
+    "revision": "d69403b5076b24d9c49d",
+    "url": "/Context-API-with-Hooks/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "0bdcee6ca9b56d40536b",
+    "url": "/Context-API-with-Hooks/static/css/main.53abeaba.chunk.css"
+  },
+  {
+    "revision": "d69403b5076b24d9c49d",
+    "url": "/Context-API-with-Hooks/static/js/2.146e56a0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Context-API-with-Hooks/static/js/2.146e56a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdcee6ca9b56d40536b",
+    "url": "/Context-API-with-Hooks/static/js/main.b0ee0e5d.chunk.js"
+  },
+  {
+    "revision": "97ea7a9babae5ae51f50",
+    "url": "/Context-API-with-Hooks/static/js/runtime-main.073acfd9.js"
+  }
+]);
